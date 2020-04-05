bdcc14ab48117b0bd4c1dc60ece641b015036c7c
Victoria M. Coutts <vzc0035@auburn.edu>
2020-04-05 14:59:03 -0500

    Added beginning comments and imported files as indicated by find_orf.py

--- translate_orf.py
+++ translate_orf.py
@@ -1,1 +1,8 @@
 #! /usr/bin/env/ python3
+# the goal of this script is to search for an open reading frame and
+#translate that open reading frame. The open reading frame that gets
+#translated will be the first one that is read by the script.
+import sys
+import re
+
+
